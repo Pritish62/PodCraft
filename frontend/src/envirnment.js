ca27b7304ef11a7456a03071fe4da6d4d242019a
Pritish Mandal <pritishkumar28@gmail.com>
2026-04-22 11:08:18 +0530

    fix error for deployed app

--- frontend/src/envirnment.js
+++ frontend/src/envirnment.js
@@ -2,7 +2,7 @@ const IS_PROD = import.meta.env.PROD
 const PROD_SERVER = process.env.VITE_API_URL;
 const DEV_SERVER = 'http://localhost:8000'
 
-const server = (IS_PROD ? PROD_SERVER : DEV_SERVER).replace(/\/+$/, '')
+const server = (IS_PROD ? PROD_SERVER : DEV_SERVER)
 
 export default server
 export { IS_PROD }
